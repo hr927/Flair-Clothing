bcc36b6941b760ca4dc3e0e05ed6db5c387311ac
Himanshu Rana <hrana927@gmail.com>
2022-11-12 17:22:12 +0530

    confirm order

---
+++ Components/cartnav.js
@@ -0,0 +1,16 @@
+const cartnav = () => {
+  return `<div id="payment_nav">
+    <div id="payment_left">
+      <i class="fa fa-lock" aria-hidden="true"></i> Secure checkout
+    </div>
+    <div id="payment_mid">
+      <h2>FLAIRCLOTHINGS</h2>
+    </div>
+    <div id="payment_right">
+      <a href="#">Need Help?</a>
+      <i class="fa fa-phone" aria-hidden="true"></i>
+      <p>(+44 (0) 20 3510 0670)</p>
+    </div>
+  </div>`;
+};
+export default cartnav;
--- Scripts/review.js
+++ Scripts/review.js
@@ -1,11 +1,15 @@
 let userDetails = JSON.parse(localStorage.getItem("userAddress"));
 let cartPrice = JSON.parse(localStorage.getItem("cartPrice")) || {};
+let paymentDetails = JSON.parse(localStorage.getItem("paymentData"));
 let subtotal = document.querySelector("#stprice");
 let delivery = document.querySelector("#delCharge");
 let total = document.querySelector("#tprice");
 delivery.innerText = "$24";
 subtotal.innerText = cartPrice.subPrice;
 total.innerText = cartPrice.total;
+import cartnav from "../Components/cartnav.js";
+let nav = document.querySelector("#nav");
+nav.innerHTML = cartnav();
 const getCart = async () => {
   try {
     let res = await fetch(`https://infinite-fortress-00447.herokuapp.com/cart`);
@@ -90,3 +94,66 @@ const getAdd = (userObj) => {
   sAdd.append(dadd, name, add, change);
 };
 getAdd(userDetails[0]);
+const getpayment = (payObj) => {
+  let paymentDiv = document.querySelector("#payment");
+  paymentDiv.innerHTML = null;
+  let pay = document.createElement("p");
+  pay.innerText = "PAYMENT METHOD";
+  let method = document.createElement("p");
+  method.innerText = "Method: ";
+  let payMethod = document.createElement("span");
+  payMethod.innerText = payObj.method;
+  method.append(payMethod);
+  let change = document.createElement("a");
+  change.href = "payment.html";
+  change.innerText = "Change";
+  if (payObj.method === "UPI") {
+    let upi = document.createElement("p");
+    upi.innerText = "UPI ID: ";
+    let upiId = document.createElement("span");
+    upiId.innerText = payObj.upi_inp_val;
+    upi.append(upiId);
+    paymentDiv.append(pay, method, upi, change);
+  }
+  if (payObj.method === "Debit / Credit Card") {
+    let name = document.createElement("p");
+    name.innerText = payObj.name;
+    let card = document.createElement("p");
+    let cardnum = "";
+    let len = payObj.number.length;
+    for (let i = len - 1; i >= len - 4; i--) {
+      cardnum += payObj.number[i];
+    }
+    card.innerText = "Card ending in " + cardnum;
+    let exp = document.createElement("p");
+    exp.innerText = "EXP:" + payObj.exp;
+    paymentDiv.append(pay, name, card, exp, change);
+  } else {
+    let cod = document.createElement("p");
+    cod.innerText = "Pay on Delivery";
+    paymentDiv.append(pay, method, cod, change);
+  }
+};
+
+getpayment(paymentDetails[0]);
+
+let confirmBtn = document.querySelector("#checkOut");
+confirmBtn.onclick = () => {
+  confirmOrder(paymentDetails[0]);
+};
+
+const confirmOrder = (el) => {
+  if (el.method === "COD") {
+    alert("Order Confirmed");
+    window.location.href = "index.html";
+  } else {
+    let otp = prompt("Enter the OTP");
+    if (otp === "123456") {
+      alert("Order Confirmed");
+      window.location.href = "index.html";
+    } else {
+      alert("Wrong OTP please try again");
+      confirmOrder(paymentDetails[0]);
+    }
+  }
+};
