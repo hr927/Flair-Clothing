cc1ad202027b2b5113f1203cff97cd82d8c90ae9
Musarraf Raza <107456526+musarraf207@users.noreply.github.com>
2022-11-12 19:51:02 +0530

    day_5

---
+++ Components/footer.js
@@ -0,0 +1,55 @@
+function footer(){
+    return `<div id="ftr">
+    <div>
+        <p>FARFETCH APP</p>
+        <p>Farfetch App for iOS and Android</p>
+    </div>
+    <div>
+        <p>DESTINATION/REGION, CURRENCY AND LANGUAGE</p>
+        <p><img id="flag" src="https://media.istockphoto.com/id/495617382/photo/indian-flag-badge.jpg?s=612x612&w=0&k=20&c=RMgOvl7nev1JhuQcW1bsAkRulBqbVChTN7i_gBiumgA=" alt="">
+            India, USD $</p>
+            <br>
+            <p>FOLLOW US</p>
+            <br>
+            <div id="lp">
+                <img src="https://scontent.fpat2-1.fna.fbcdn.net/v/t39.30808-6/313435269_1764951673879962_2767049279257891871_n.jpg?stp=cp0_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=mC5vQQTAixkAX8FjOT6&_nc_ht=scontent.fpat2-1.fna&oh=00_AfCgWXZds8zlwH3ahsEwjUNMYiTdgJSt6KVDXYiAjWTqFQ&oe=6371317D" alt="">
+                <img src="https://scontent.fpat2-3.fna.fbcdn.net/v/t39.30808-6/313425054_1764951653879964_4392626476403129399_n.jpg?stp=cp0_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=qaI9G2A39gkAX81dzH-&_nc_ht=scontent.fpat2-3.fna&oh=00_AfCejfHq5F593kC5gz-8HEkAdarte0gR6EAcEws99IIskA&oe=63716AB1" alt="">
+                <img src="https://scontent.fpat2-2.fna.fbcdn.net/v/t39.30808-6/313425246_1764951663879963_5668423944383030283_n.jpg?stp=cp0_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=uQApWX2hbI0AX8Ji9o8&_nc_ht=scontent.fpat2-2.fna&oh=00_AfAg76dxtZO4vt_fhq-nU0u7vQDJ22HT9pxaP0fW-cMuvg&oe=6371EFEE" alt="">
+                <img src="https://scontent.fpat2-1.fna.fbcdn.net/v/t39.30808-6/313441773_1764951717213291_8552256118187345891_n.jpg?stp=cp0_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=hDI2UHtjq1oAX_eyklM&_nc_ht=scontent.fpat2-1.fna&oh=00_AfA4lO9xaOAZN6LVNINMMKe3uD9QQZ1EsVj39iFaj7N1cg&oe=63721740" alt="">
+                <img src="https://scontent.fbom19-1.fna.fbcdn.net/v/t39.30808-6/314108047_1764951733879956_3173910613140916675_n.jpg?stp=cp0_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=N-X6lJ-fgmUAX8MC1zD&_nc_oc=AQm8f5ckPbwZ0P--C5hwnHxq9WnOO7uEUD9ielhYfmQt9U66jqs1lAfXhGo2fqFTngMu6htvSHKLD5_M5Og0sAJ4&_nc_ht=scontent.fbom19-1.fna&oh=00_AfCAQXVRl4j4_Z7EBf77a_TMX3Nujfk9Yds4ZKzvUXKU6w&oe=63725C9D" alt="">
+                <img src="https://scontent.fpat2-3.fna.fbcdn.net/v/t39.30808-6/315256610_1764951757213287_5867011085631492473_n.jpg?stp=cp0_dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=cxUyxq5Eph4AX9ZJR1y&_nc_ht=scontent.fpat2-3.fna&oh=00_AfA91AMHX9W8gScwC_PxwRIEoUMIXGZljXU1WCdF9qdSKw&oe=63711D70" alt="">
+            </div>
+    </div>
+    <div id="c3p">
+        <div>
+            <p>CUSTOMER SERVICE</p>
+            <p>CUSTOMER SERVICE</p>
+            <p>Orders & delivery</p>
+            <p>Payment & pricing</p>
+            <p>Cryptocurrency payments</p>
+            <p>Returns & refunds</p>
+            <p>FAQs</p>
+            <p>Terms & conditions</p>
+            <p>Promotion terms & conditions</p>
+            <p>Privacy policy</p>
+            <p>Accessibility</p>
+        </div>
+        <div>
+            <p>ABOUT FARFETCH</p>
+            <p>About Us</p>
+            <p>Investors</p>
+            <p>Boutique Partners</p>
+            <p>Affiliate Programme</p>
+            <p>Student discount UNiDAYS</p>
+            <p>Youth discount</p>
+            <p>Careers</p>
+            <p>Customer Promise
+            </p>
+            <p>FARFETCH App</p>
+            <p>Sitemap</p>
+            <p>Positively FARFETCH</p>
+        </div>
+    </div>
+    </div>`
+}
+export default footer
